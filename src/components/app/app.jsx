889c2d02e0f0b45b7051cf5095b09cb2067d84b7
Akira <154.4k1r4@gmail.com>
2019-05-10 03:41:50 +0600

    NEW: Настроено визуальное представление приложения — App

---
+++ src/components/app/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MainView from '../main-view/main-view.jsx';
+
+/**
+ * Визуальное представление приложения.
+ *
+ * @function App
+ * @return {object} — react component
+ */
+const App = () => {
+  return <MainView/>;
+};
+
+export default App;
